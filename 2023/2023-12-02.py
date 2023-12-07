c582802ed194270e0ee7e0c9c707342c53f92b20
vdiaz <vadrienzo@gmail.com>
2023-12-07 14:57:43 +0000

    2023-12-02 part1

---
+++ 2023/2023-12-02.py
@@ -0,0 +1,47 @@
+import argparse
+import pandas as pd
+
+
+def split_colours_into_integers(df: pd.DataFrame) -> pd.DataFrame:
+    # Splits the text into a list of integers for each color:
+    df = df.copy()
+    for color in ["red", "blue", "green"]:
+        df[color] = df.record_of_games.str.findall(rf"(\d+ {color})").apply(
+            lambda x: [int(item.replace(f' {color}', "")) for item in x]
+        )
+    return df
+
+
+def part_1(df: pd.DataFrame) -> pd.DataFrame:
+    mask = (
+        df.red.apply(lambda x: all([item <= 12 for item in x]))
+        & df.blue.apply(lambda x: all([item <= 14 for item in x]))
+        & df.green.apply(lambda x: all([item <= 13 for item in x]))
+    )
+    return df[mask]
+
+
+def main(args: argparse.Namespace):
+    ## Part One --------------------
+    data = pd.read_csv(f"{args.data_dir}/day-2-part1.csv", sep=":").assign(
+        gameid=lambda x: x.gameid.str.replace("Game ", "").astype(int),
+    )
+    splited_data = split_colours_into_integers(data)
+    # print("Full data\n")
+    # print(splited_data.head())
+    print("Part One --------------------")
+    part1 = part_1(splited_data)
+    # print("Masked data\n")
+    # print(part1.head())
+    print(f"Sum of ID games: {part1.gameid.sum()}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "data_dir",
+        help="Directory containing data. Defaults to testdata.",
+        default="testdata",
+    )
+    args = parser.parse_args()
+    main(args)
